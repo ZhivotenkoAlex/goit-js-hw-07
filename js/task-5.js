61986202cbc1d9c9d0546afb756585c121465e2a
Zhivotenko Aleksandr <zhivotenko.a.s@gmail.com>
2020-09-20 19:45:53 +0300

    фиксит флексы

--- js/task-5.js
+++ js/task-5.js
@@ -2,7 +2,10 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output")
 
 const greeting = (event) => {
-    output.textContent = event.currentTarget.value;
+    output.textContent = event.currentTarget !== "" ? event.currentTarget.value : "Незнакомец";
+
+
+    // output.textContent = event.currentTarget.value;
 }
 
 input.addEventListener("input", greeting);
